0ce81267f6b67e36524ad0131641c631b82f3799
Akshit203 <akshitgupta2003.ak@gmail.com>
2024-02-27 23:19:42 +0530

    Updated quickSort.java

--- quickSort.java
+++ quickSort.java
@@ -1,3 +1,24 @@
 public class quickSort {
-    
+    public static void main (String args[]){
+
+
+
+
+    }
+
+    public static void split_Array (int low, int high){
+
+        int pivot ;
+    }
+
+    public static void quickSort_function (int low, int high){
+
+        if (low < high){
+
+            j = split_Array (low, high);
+
+            quickSort_function(low, high);
+            quickSort_function(j + 1, high);
+        }
+    }
 }
